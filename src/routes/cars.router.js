f35159414743acdff4ead1ae91ed0ab6c44924ee
Gabriel Escarate Prieto <gabrielescarate33@gmail.com>
2023-11-09 18:09:01 -0500

    segunda entrega mongo

--- src/routes/cars.router.js
+++ src/routes/cars.router.js
@@ -59,4 +59,95 @@ catch(error){
     res.status(500).send({error:error.message});}
 });
 
+
+router.delete("/:cid", async (req,res)=>{
+    try {
+    const {cid} =req.params;
+    const cart = await cartManager.getCartById(cid);
+    if (!cart){
+            return res.status(404).send({status:"error",message:"Cart not found"})
+        }
+    const result = await cartManager.delete(cid);
+    res.status(200).send({status:"success",payload:result});
+    }
+    catch(error) {
+    res.status(500).send({status:"error",message:error.message})}
+    });
+
+router.delete('/:cid/product/:pid', async (req,res)=>{
+        try{
+            const cid =req.params.cid;
+            const pid =req.params.pid;
+            const cart = await cartManager.getCartById(cid);
+            const product = await productManager.getProductById(pid);
+            if (!cart){
+                    return res.status(404).send({status:"error",message:"Cart not found"})
+                }
+            if (!product){
+                    return res.status(404).send({status:"error",message:"Product not found"})
+                }
+            
+            if (cart.products.length!==0){
+            const indexProductInCart = cart.products.findIndex(product=>product.product._id.toString()===pid)
+           
+                if (indexProductInCart!==-1){
+                    cart.products.splice(indexProductInCart,1);
+                        } 
+            }            
+            const result = await cartManager.update(cid,{"products": cart.products});
+            res.status(200).send({status:"success",payload:result});
+            
+                
+                }
+        catch(error){
+            console.log(error.message);
+            res.status(500).send({error:error.message});}
+        })
+        
+router.put("/:cid", async (req,res)=>{
+            try {
+            const {products} = req.body;
+            const {cid} =req.params;
+            if (!products) {
+            return res.status(400).send({status:"error", message:"incomplete values"})};
+            const result = await cartManager.update(cid,{"products": products})
+            res.status(201).send({status:"success",payload:result});
+            }
+            catch(error) {
+            res.status(500).send({status:"error",message:error.message})}
+            });
+
+router.put('/:cid/products/:pid', async (req,res)=>{
+                try{
+                    const cid =req.params.cid;
+                    const pid =req.params.pid;
+                    // const {cid, pid} = req.params;
+                    const amount = req.body;
+                    const cart = await cartManager.getCartById(cid);
+                    const product = await productManager.getProductById(pid);
+                    if (!cart){
+                            return res.status(404).send({status:"error",message:"Cart not found"})
+                        }
+                    if (!product){
+                            return res.status(404).send({status:"error",message:"Product not found"})
+                        }
+                    if (cart.products.length===0){
+                        cart.products.push({"product":pid,"quantity":amount.quantity})
+                    } else{
+                    const indexProductInCart = cart.products.findIndex(product=>product.product._id.toString()===pid)
+                        if (indexProductInCart!==-1){
+                            cart.products[indexProductInCart].quantity+=amount.quantity;
+                                } else {
+                                    cart.products.push({"product":pid,"quantity":amount.quantity});
+                                };
+                            }            
+                    const result = await cartManager.update(cid,{"products": cart.products});
+                    res.status(201).send({status:"success",payload:result});
+                        }
+                catch(error){
+                    console.log(error.message);
+                    res.status(500).send({error:error.message});}
+                })
+
+
 export default router;
